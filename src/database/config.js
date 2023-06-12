74c366d76bfa1fb4a074345c297174f19328d326
Noel <noelhorta88@outlook.com>
2023-06-12 13:02:23 -0600

    bankapp

---
+++ src/database/config.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require('sequelize');
+
+const db = new Sequelize({
+  // dialect: process.env.DB_DIALECT,
+  // host: process.env.DB_HOST,
+  // username: process.env.DB_USERNAME,
+  // password: process.env.DB_PASSWORD,
+  // // port: process.env.PORT2,
+  // logging: false,
+
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  database: 'bankappdb',
+  password: 'root',
+  logging: false,
+});
+
+module.exports = { db };
---
+++ src/models/users.model.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require('sequelize');
+const { db } = require('../database/config');
+
+const User = db.define('users', {
+  id: {
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  accountNumber: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: () =>
+      Math.floor(Math.random() * (999999 - 100000 + 1) + 100000),
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  amount: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 1000,
+  },
+  status: {
+    type: DataTypes.ENUM('active', 'disabled'),
+    allowNull: false,
+    defaultValue: 'active',
+  },
+});
+
+module.exports = User;
